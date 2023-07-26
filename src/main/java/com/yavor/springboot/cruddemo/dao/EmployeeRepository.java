99ba2059ce997c91f8351e9422f1720449f17ba8
yavor <jm87585@gmail.com>
2023-07-26 08:11:24 +0300

    Create Employee Rest API using Spring Data REST and Spring Data JPA

---
+++ src/main/java/com/yavor/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.yavor.springboot.cruddemo.dao;
+
+import com.yavor.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "employees")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> { }
